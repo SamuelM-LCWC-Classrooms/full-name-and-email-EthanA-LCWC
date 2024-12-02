42c8080aa46c442af6348299fddb2bec8aaab3a1
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-12-02 13:32:04 +0000

    I forgot to make the email lowercase

--- employee.py
+++ employee.py
@@ -3,12 +3,4 @@ class Employee:
         self.firstname = firstname
         self.lastname = lastname
         self.fullname = f"{firstname} {lastname}"
-        self.email = f"{firstname}.{lastname}@company.com"
-
-#emp_1 = Employee("John", "Smith")               
-#emp_2 = Employee("Mary",  "Sue")             
-#emp_3 = Employee("Antony", "Walker")
-                  
-#print(emp_1.fullname)
-#print(emp_2.email)
-#print(emp_3.firstname)
+        self.email = f"{firstname}.{lastname}@company.com".lower()
